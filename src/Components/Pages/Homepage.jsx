56f0ff3e1f7feac17f9767f2705eca80f02224fe
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-18 14:44:32 +0100

    deleted some files and added navigation

---
+++ src/Components/Pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import { Heading, Box } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Homepage() {
+    return(
+        <Box>
+            <Heading>Yeah we out here just to check the error filled code and see how dhope</Heading>
+        </Box>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
 );
