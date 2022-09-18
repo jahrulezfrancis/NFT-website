7e57382baa7e806da68b72d835fcd4c2a2f807a6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-18 16:11:33 +0100

    Added sidebar

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,11 +1,31 @@
-import { Box, Flex, UnorderedList, ListItem, Image, Input, InputGroup, InputRightElement, Button, Avatar, HStack } from '@chakra-ui/react';
+import { Box, Icon, Flex, UnorderedList, ListItem, Image, Input, InputGroup, InputRightElement, Button, Avatar, HStack, VStack, Spacer } from '@chakra-ui/react';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FiSearch } from "react-icons/fi"
-import { MdAdd } from "react-icons/md"
+import { BiNews } from "react-icons/bi"
+import { MdAdd, MdExplore } from "react-icons/md"
 import HeaderLogo from "../Assests/Images/header-logo.png"
 import UserAvatar from "../Assests/Images/user-avatar.png"
+import { GrServicePlay } from "react-icons/gr"
+import { GiNetworkBars } from "react-icons/gi"
 
+
+
+export function SideBar() {
+    return (
+        <Box>
+            <Flex direction='column' gap='5' justify='center' align='center' w='7em' p='3em'>
+                <Icon color='#1F50FF' w='2.5em' h='2.5em' as={MdExplore} />
+                <Spacer />
+                <Icon  w='2.5em' h='2.5em' as={GiNetworkBars} />
+                <Spacer />
+                <Icon  w='2.5em' h='2.5em' as={GrServicePlay} />
+                <Spacer />
+                <Icon  w='2.5em' h='2.5em' as={BiNews} />
+            </Flex>
+        </Box>
+    )
+}
 
 
 export default function Navigation() {
@@ -38,3 +58,5 @@ export default function Navigation() {
         </Box>
     )
 }
+
+
