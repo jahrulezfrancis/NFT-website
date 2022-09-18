a6a87ff1340cbe8e8ec5efa5681e7c7eab3a5b8d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-18 15:31:47 +0100

    removed nagation source code and added as import

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -1,36 +1,13 @@
-import { Heading, Box, Flex, UnorderedList, ListItem, Image, Input, InputGroup, InputRightElement, Button, Avatar, HStack } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { FiSearch } from "react-icons/fi"
-import { MdAdd } from "react-icons/md"
-import HeaderLogo from "../Assests/Images/header-logo.png"
-import UserAvatar from "../Assests/Images/user-avatar.png"
+import Navigation from '../Header/Navigation';
+
 
 
 export default function Homepage() {
     return (
         <Box>
-            <UnorderedList listStyleType='none'>
-                <Flex align='center' justify='space-between' p='1em'>
-                    <ListItem>
-                        <NavLink><Image src={HeaderLogo} alt='Header logo' /> </NavLink>
-                    </ListItem>
-                    <ListItem>
-                        <InputGroup w='30em'>
-                            <InputRightElement children={<FiSearch />} />
-                            <Input type='search' placeholder='most searched for: Hip Hop' />
-                        </InputGroup>
-                    </ListItem>
-                    <HStack>
-                        <ListItem>
-                            <Button bgColor='#1F50FF' color='white' fontFamily='Hyundai Sans Head Office' _hover={{ bgColor: '#6F75F7' }} leftIcon={<MdAdd />}>Create</Button>
-                        </ListItem>
-                        <ListItem>
-                            <Avatar name='Francis Longsaar' src={UserAvatar} />
-                        </ListItem>
-                    </HStack>
-                </Flex>
-            </UnorderedList>
+           <Navigation />
         </Box>
     )
 }
