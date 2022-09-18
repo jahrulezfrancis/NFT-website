2e8da58a832c5500d4cc6ba34ece153fb746407d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-18 15:14:27 +0100

    Initial commit

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -1,10 +1,36 @@
-import { Heading, Box } from '@chakra-ui/react';
+import { Heading, Box, Flex, UnorderedList, ListItem, Image, Input, InputGroup, InputRightElement, Button, Avatar, HStack } from '@chakra-ui/react';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FiSearch } from "react-icons/fi"
+import { MdAdd } from "react-icons/md"
+import HeaderLogo from "../Assests/Images/header-logo.png"
+import UserAvatar from "../Assests/Images/user-avatar.png"
+
 
 export default function Homepage() {
-    return(
+    return (
         <Box>
-            <Heading>Yeah we out here just to check the error filled code and see how dhope</Heading>
+            <UnorderedList listStyleType='none'>
+                <Flex align='center' justify='space-between' p='1em'>
+                    <ListItem>
+                        <NavLink><Image src={HeaderLogo} alt='Header logo' /> </NavLink>
+                    </ListItem>
+                    <ListItem>
+                        <InputGroup w='30em'>
+                            <InputRightElement children={<FiSearch />} />
+                            <Input type='search' placeholder='most searched for: Hip Hop' />
+                        </InputGroup>
+                    </ListItem>
+                    <HStack>
+                        <ListItem>
+                            <Button bgColor='#1F50FF' color='white' fontFamily='Hyundai Sans Head Office' _hover={{ bgColor: '#6F75F7' }} leftIcon={<MdAdd />}>Create</Button>
+                        </ListItem>
+                        <ListItem>
+                            <Avatar name='Francis Longsaar' src={UserAvatar} />
+                        </ListItem>
+                    </HStack>
+                </Flex>
+            </UnorderedList>
         </Box>
     )
 }
