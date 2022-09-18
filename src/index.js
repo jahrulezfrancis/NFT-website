64bb1d5180149abb396ee7f95ce8f90d006afea8
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-18 15:30:24 +0100

    updated react-dom import source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
