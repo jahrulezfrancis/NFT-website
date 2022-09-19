34d4723210fae56120dee008a8fa5706cc63b310
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-19 18:39:53 +0100

    Added the card component

--- src/Components/Pages/Homepage.jsx
+++ src/Components/Pages/Homepage.jsx
@@ -2,18 +2,19 @@ import { Box, Flex } from '@chakra-ui/react';
 import React from 'react';
 import HeaderWelcome from '../Header/HeaderIntro';
 import Navigation, { SideBar } from '../Header/Navigation';
+import NFTcards from '../Interface/Card';
 
 
 
 export default function Homepage() {
     return (
         <Box>
-           <Navigation />
-           <Flex>
-
-           <SideBar />
-           <HeaderWelcome />
-           </Flex>
+            <Navigation />
+            <Flex>
+                <SideBar />
+                <HeaderWelcome />
+            </Flex>
+                <NFTcards />
         </Box>
     )
 }
